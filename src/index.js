ddb2213bb61c1a5aa2418689f9bdf6e977936f0a
Alex <aleksey.novikov@tatar.ru>
2021-11-15 14:44:49 +0300

    homeWork rollup

--- src/index.js
+++ src/index.js
@@ -5,18 +5,10 @@ console.log('hello world')
 
 const textInContainer = document.querySelector('.container')
 
-
 const mainText = document.createElement('div')
 mainText.className = 'main-text'
 mainText.innerHTML = '<h2>I love JavaScript</h2>'
 textInContainer.append(mainText)
-
-const homeWork = document.createElement('div')
-homeWork.className = 'home-work'
-homeWork.textContent = 'Домашняя работа по Rollup'
-
-textInContainer.append(homeWork)
-
 
 const jsImageHTML = document.createElement('img')
 jsImageHTML.className = 'js-image'
